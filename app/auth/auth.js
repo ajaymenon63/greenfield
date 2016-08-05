9bea663c9013a63c637c479849412739bacb33b8
ajaymenon63 <ajaymenon63@gmail.com>
2016-08-05 10:26:18 -0700

    (frontend) add form.ejs diplaying form application fro owner and dog

--- app/auth/auth.js
+++ app/auth/auth.js
@@ -1,2 +1,27 @@
-auth.js
-//working on auth.js
+angular.module('puppyLove.auth', [])
+
+.controller('AuthController', function ($scope, $window, $location, Auth) {
+  $scope.user = {};
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.puppyLove', token);
+        $location.path('/profile');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.puppyLove', token);
+        $location.path('/profile');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+});
